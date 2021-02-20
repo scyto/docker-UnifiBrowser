4ab8855df795161c105d180191bef85642b94878
scyto <lx_b@msn.com>
2021-02-20 15:46:00 -0800

    commit no auth changes

--- multi-arch-linx-build.ps1
+++ multi-arch-linx-build.ps1
@@ -1,1 +1,1 @@
-docker buildx build -t scyto/unifibrowser:noauth --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 .
+docker buildx build -t scyto/unifibrowser:latest --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6 .
